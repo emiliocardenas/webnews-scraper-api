9fc51610d3d6541d86f112ff1c99d310608478ab
Emilio Cardenas <ecarden9@stedwards.edu>
2022-08-20 00:35:23 -0500

    structure change, stock news

---
+++ sources.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        name: 'nyp',
+        address: 'https://nypost.com/business/',
+        base: ''
+    },
+    {
+        name: 'fintimes',
+        address: 'https://www.ft.com/',
+        base: ''
+    },
+    {
+        name: 'nytimes',
+        address: 'https://www.nytimes.com/section/business',
+        base: ''
+    },
+    {
+        name: 'yahooFinance',
+        address: 'https://finance.yahoo.com/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAJQBSP36mCJgLU9y681i3zi9_qWXiI4KUoB8b8LQt_eSJUyABBVe4A9w3v3NVufc2-5F31sEJvCEjdzzO8x7KXHPDT5_k3x3l3ZNciY_wGz1k2gK6gPJZp3EH0cYbRfEhZHpkfhInEGCUIl1vvfK8kMoaebH1ihI0qaGtSnYPBIW',
+        base: ''
+    },
+    {
+        name: 'cnbc',
+        address: 'https://www.cnbc.com/finance/',
+        base: ''
+    },
+    // {
+    //     name:   ,                             // Add more news sources if wanted!
+    //     address:   ,
+    //     base: ''
+    // },
+]
